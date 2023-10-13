759e0183a8c300e802b594668010e38153f8e7f4
HannahMcCabe31 <hmccabe.hm@gmail.com>
2023-10-13 16:03:24 +0100

    invalid request post in progress

--- todos/todos.test.js
+++ todos/todos.test.js
@@ -44,15 +44,20 @@ test("GET /api/todos", async function () {
 test("POST /api/todos", async function () {
   // await the 'resetAllTables' function
   await resetAllTables(seedData);
-
+   
+  // Declare a 'requestBody' variable that represents the actual request body
+  const requestBody = { task: "Eat chocolate", completionDate: "2023-10-13" };
+  
   //    Send a post request to the /api/todos endpoint
   //    call `request` and pass in the Express app as an argument
   //    await the overall expression and assign it to a `response` constant
   const response = await request(app)
     .post("/api/todos")
-    .send({ task: "Eat chocolate", completionDate: "2023-10-13" })
+    .send(requestBody)
     .set("Accept", "application/json");
 
+
+  
   // console.log(response.body);
 
   // Declare a variable to store payload within the response body
@@ -61,8 +66,7 @@ test("POST /api/todos", async function () {
   // Assertions:
   // Request body = { task: string, completionDate: string },
 
-  // Declare a 'requestBody' variable that represents the actual request body
-  const requestBody = { task: "Eat chocolate", completionDate: "2023-10-13" };
+
 
   // Comparing the actual request body with the expected data type
   expect(typeof requestBody.task && typeof requestBody.completionDate).toBe(
@@ -106,3 +110,32 @@ test("DELETE /api/todos/2", async function () {
   // Response header	Content-Type header should contain application/json
   expect(response.header["content-type"]).toMatch("application/json");
 });
+
+//Create invalid request for post
+test("invalid POST request", async function () {
+  //await resetAllTables function
+  await resetAllTables(seedData);
+
+  // Declare a 'requestBody' variable that represents the actual request body
+  const requestBody = {};
+
+  //    Send a post request to the /api/todos endpoint
+  //    call `request` and pass in the Express app as an argument
+  //    await the overall expression and assign it to a `response` constant
+  const response = await request(app)
+    .post("/api/todos")
+    .send(requestBody)
+    .set("Accept", "application/json");
+
+  // Check the request body 
+  // assert missing / invalid request body 
+  expect(Object.keys(requestBody).length).toBe(0);
+
+
+  // Request header: Content-Type header should contain application/json,
+  // To assert the Content-Type header, we need to access the headers of the request object and check if it contains the expected value.
+  const requestHeaders = { "Content-Type": "application/json" };
+  const expectedContentType = "application/json";
+
+  expect(requestHeaders["Content-Type"]).toBe(expectedContentType);
+})
